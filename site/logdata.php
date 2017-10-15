0f4beaf90e792637dc769d0d69419b36e411c539
duicul <duicul@yahoo.com>
2017-10-15 13:20:45 +0300

    scriptfolder

--- site/logdata.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();
-   if(isset($_SESSION['user'])&&isset($_SESSION['name'])&&isset($_SESSION['mail']))
-echo $_SESSION['user']." ".$_SESSION['name']." ".$_SESSION['mail'];
-   else echo "anonymous";
-?>
